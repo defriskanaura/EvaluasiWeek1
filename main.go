84f317b02178a773ed6f281e1ba733afe2284edb
defriskanaura <defriskanaura@gmail.com>
2024-02-03 19:06:00 +0700

    adding new function wordplayone

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// mengurutkan abjad a hingga z
+// tiap abjad urutan kelipatan 5 diganti dengan tanda "%"
+func wordplayOne() {
+	for i := 'a'; i <= 'z'; i++ {
+		if (i-'a'+1)%5 == 0 {
+			fmt.Print("% ")
+		} else {
+			fmt.Printf("%c ", i)
+		}
+	}
+	fmt.Println("")
+}
+
 func main() {
 	fmt.Println("Selamat datang di WordPlay")
 }
