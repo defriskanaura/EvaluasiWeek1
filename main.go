0434f94c2637161bb07d9bd6e3d126aae69509ac
defriskanaura <defriskanaura@gmail.com>
2024-02-03 19:31:10 +0700

    adding new function wordplaytwo

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // mengurutkan abjad e hingga x
 // tiap abjad urutan kelipatan 2 diganti dengan tanda "re"
@@ -15,7 +18,23 @@ func wordplayOne() {
 	fmt.Println("")
 }
 
+// menyebutkan bee sebanyak 15 kali
+// tiap bee urutan kelipatan 3 diubah menggunakan uppercase
+func wordplayTwo() {
+	for i := 1; i <= 15; i++ {
+		word := "bee"
+		if i%3 == 0 {
+			// Ubah huruf pertama menjadi uppercase
+			fmt.Printf("%s ", strings.ToUpper(word))
+		} else {
+			fmt.Printf("%s ", word)
+		}
+	}
+	fmt.Println("")
+}
+
 func main() {
 	fmt.Println("Selamat datang di WordPlay")
 	wordplayOne()
+	wordplayTwo()
 }
